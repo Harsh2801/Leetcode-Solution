fea7a7ca9400a6a6ab269384b70c2780094c9115
Harsh2801 <55402521+Harsh2801@users.noreply.github.com>
2022-05-21 11:03:03 +0530

    Time: 3 ms (94.89%), Space: 10.6 MB (38.45%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -1,27 +1,55 @@
 class Solution {
 public:
-	string simplifyPath(string s) {
-		vector<string> p;
-		for(int i = 0; i < s.size(); i++) {
-			if(s[i] == '/') {
-				string temp;
-				int l = i + 1;
-				while(l < s.size() && s[l] != '/') {
-					temp.push_back(s[l]);
-					l++;
-				}
-				if(temp.empty() || temp == ".")
-					continue;
-				if(temp == "..") {
-					if(!p.empty())  p.pop_back();
-				}
-				else 
-					p.push_back(temp);
-			}
-		}
-		string ans;
-		for(auto& str : p) 
-			ans += "/" + str;
-		return (ans.empty() == true) ? "/" : ans; 
-	}
+    string simplifyPath(string path) {
+       // we are using stack becase we have to pop out the most recent file when we see '..'
+        stack<string>st;
+        string ans = "";
+        string temp = "";
+        int n = path.length();
+  // if path does not end with the '/' then we have to add it without adding it otherwise 
+  // we will not able to add the last temp string because we dont find / before i will reached to n 
+        if(path[n-1]!='/')
+        {
+            path += '/';
+            n++;
+        }
+        int i = 1; // we will start from index 1 
+        while(i<n)
+        {
+            if(path[i]=='/')
+            {
+                if(temp=="" || temp== ".")
+                {
+                    //ignor it
+                }
+                else if(temp== "..")
+                {
+                    if(!st.empty())
+                    st.pop();
+                }
+                else
+                {
+                    st.push(temp);
+                } 
+             temp = "";
+            }
+            else
+            {
+                temp += path[i];
+            }
+            i++;
+        }
+        // make the canonical string through poping the stack
+        while(!st.empty())
+        {
+            ans = '/'+st.top()+ans;
+            st.pop();
+        }
+        // it handel the case when we dont find anything as ans 
+        if(ans=="")
+        {
+            ans += '/';
+        }
+     return ans;
+    }
 };
