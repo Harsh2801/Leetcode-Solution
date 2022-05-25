aaa1c47932c6b5e78be6712f0394570329eab61c
Harsh2801 <55402521+Harsh2801@users.noreply.github.com>
2022-05-26 01:50:13 +0530

    Time: 232 ms (84.41%), Space: 84.2 MB (63.03%) - LeetHub

---
+++ 901-online-stock-span/901-online-stock-span.cpp
@@ -0,0 +1,26 @@
+class StockSpanner {
+public:
+    
+    //pair.first denotes the price 
+	//pair.second denotes the number of stocks less than equal to the current price
+    stack<pair<int,int>> st;
+    
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int span=1;            //since current stock is also counted
+        
+        //get the previous stocks which are less than or equal to the  current price
+        while(!st.empty() && st.top().first<=price)
+        {
+            span += st.top().second;
+            st.pop();
+        }
+        
+        st.push({price,span});
+        return span;
+        
+    }
+};
