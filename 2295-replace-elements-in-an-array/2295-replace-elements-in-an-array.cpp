58510c796c5d9dd837f8f24d7d2ecd58426f6186
Harsh2801 <55402521+Harsh2801@users.noreply.github.com>
2022-06-05 19:41:01 +0530

    Time: 575 ms (42.86%), Space: 122.9 MB (100.00%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
@@ -1,3 +1,11 @@
+// We are using array of size 10^6 (maximum range of a[i] as per contraints).
+
+// First of all we are storing the position of each element ( they are unique )
+// After each operation we find position of first element in operation using array m and change element in a on that position with second element of operation
+// store the position of new element in m
+
+
+
 class Solution {
 public:
        vector<int> arrayChange(vector<int> a, vector<vector<int>>& op) {
