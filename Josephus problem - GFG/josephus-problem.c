90402641a5dd564fe024ba1d612a6efea19402a9
Harsh2801 <55402521+Harsh2801@users.noreply.github.com>
2022-06-06 13:22:32 +0530

    Added solution - LeetHub

---
+++ Josephus problem - GFG/josephus-problem.c
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+int josephus(int n, int k)
+{
+   //Your code here
+   if ( n==1) 
+ return 1 ;
+ int r =  (josephus(n-1,k)+k)%n;
+  if (r == 0)
+  return n ;
+  else return r;
+}
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	scanf("%d", &t);//testcases
+	while(t--)
+	{
+		int n,k;
+		scanf("%d%d", &n, &k);//taking input n and k
+		
+		//calling josephus() function
+		printf("%d\n", josephus(n,k));
+	}
+	return 0;
+}  // } Driver Code Ends
