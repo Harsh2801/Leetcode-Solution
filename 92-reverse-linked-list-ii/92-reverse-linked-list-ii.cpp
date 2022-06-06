4d49a6618a4b05215735f45b91ab4590f5ebdeca
Harsh2801 <55402521+Harsh2801@users.noreply.github.com>
2022-06-06 11:37:10 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (64.14%) - LeetHub

---
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+//  */
+// class Solution {
+// public:
+//   ListNode* reverseBetween(ListNode* head, int left, int right) {
+//         if(head==NULL || right==left){
+//             return head;
+//         }
+//         ListNode* dummy=new ListNode(-1);
+//         dummy->next=head;
+//         int count=0;
+//         ListNode* curr=dummy;
+//         ListNode* prev=NULL;
+//         while(count<left){
+//             prev=curr;
+//             curr=prev->next;
+//             count++;
+//         }
+//         ListNode* NewPrev=prev;
+//         ListNode* NewCurr=curr;
+//         while(left<=right){
+//             ListNode* temp=NewCurr->next;
+//             NewCurr->next=NewPrev;
+//             NewPrev=NewCurr;
+//             NewCurr=temp;
+//             left++;
+//         }
+//         prev->next=NewPrev;
+//         curr->next=NewCurr;
+//         return dummy->next;
+//     }
+// };
+class Solution {
+public:
+  ListNode* reverseBetween(ListNode* head, int left, int right) {
+       ListNode *dummy = new ListNode(0), *pre = dummy, *cur;
+       dummy -> next = head;
+       for (int i = 0; i < left - 1; i++) {
+           pre = pre -> next;
+       }
+       cur = pre -> next;
+       for (int i = 0; i < right - left; i++) {
+           ListNode* temp = pre -> next;
+           pre -> next = cur -> next;
+           cur -> next = cur -> next -> next;
+           pre -> next -> next = temp;
+       }
+       return dummy -> next;
+   }
+};
