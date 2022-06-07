323132c2e7986e6731e8ff558d6bc8ca1b737566
Harsh2801 <55402521+Harsh2801@users.noreply.github.com>
2022-06-07 22:43:35 +0530

    Time: 14 ms (43.64%), Space: 11.5 MB (17.49%) - LeetHub

---
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,68 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
+//         if(!head) return NULL;
+        
+//         Node* temp = head;
+        
+//         while(temp){
+//             Node* node = new Node(temp->val);
+//             node->next = temp->next;
+//             temp->next = node;
+//             temp = node->next;
+//         }
+        
+//         temp = head;
+//         while(temp){
+//             temp->next->random = (temp->random)?temp->random->next:NULL;
+//             temp = temp->next->next;
+//         }
+        
+//         Node* ans = head->next;
+//         temp = head->next;
+//         while(head){
+//             head->next = temp->next;
+//             head = head->next;
+//             if(!head) break;
+//             temp->next = head->next;
+//             temp = temp->next;
+//         }
+//         return ans;
+//     }
+// };
+
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        map<Node*, Node*> m;
+      //  int i=0;
+        Node* ptr = head;
+        while (ptr) {
+            m[ptr] =new Node(ptr->val);
+            ptr = ptr->next;
+        }
+        ptr = head;
+        while (ptr) {
+            m[ptr]->next = m[ptr->next];
+            m[ptr]->random = m[ptr->random];
+            ptr = ptr->next;
+        }
+        return m[head];
+    }
+};
