e9c0da20cdfa6bc6b6b85b507a4b0f90d98ef183
Harsh2801 <55402521+Harsh2801@users.noreply.github.com>
2022-06-10 16:06:24 +0530

    Time: 58 ms (9.56%), Space: 28.2 MB (31.21%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -14,15 +14,15 @@ public:
        int global=INT_MIN;
     int solve(TreeNode* root) {
         if(root==NULL)return 0;
-        int lst = solve(root->left);
-        int rst = solve(root->right);
-        int case1=max({max(lst, rst)+root->val, root->val,lst+rst+root->val});
-        global=max(case1, global);
-        int pass = root->val+max(lst, rst);
-        return pass>0?pass:0;//we return case1 here because this is the only  case which can extend the maxpath to other nodes
+        int l = solve(root->left);
+        int r = solve(root->right);
+        int case1=max(max(l, r)+root->val, root->val);
+        int case2= max(case1, l+r+root->val);
+        global=max(case2, global);
+        return case1;//we return case1 here because this is the only  case which can extend the maxpath to other nodes
     }
-    int maxPathSum(TreeNode* root){
-        solve(root);
+    int maxPathSum(TreeNode* r){
+        solve(r);
         return global;
     }
 };
