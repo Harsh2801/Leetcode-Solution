0ba35bc264a6c8c7d12f8ab213e9cf15efc182f7
Harsh2801 <55402521+Harsh2801@users.noreply.github.com>
2022-06-22 11:57:41 +0530

    Time: 10 ms (71.97%), Space: 10.5 MB (9.65%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -5,58 +5,58 @@ public:
     //     int s=nums.size()-k;
     //     return nums[s];
     // }
-// int findKthLargest(vector<int>& nums, int k) 
-//     {
-//         // Take a max heap
-//         priority_queue<int> q;
-//         // Insert element
-//         // Element would be inserted in a manner that lagest element is at the top
-//         for(int i=0;i<nums.size();i++)
-//         {
-//             q.push(nums[i]);
-//         }
-//         // Take out the element k-1 times and kth element will be the required element
-//         for(int i=0;i<k-1;i++)
-//         {
-//             q.pop();
-//         }
-//         //return that element
-//         return q.top();
-//     }
-
-    
-    int fun(int x,vector<int>& nums,int n){
-        int count = 0;
-        for(int i=0;i<n;i++){
-            if(nums[i] <= x)
-                count++;
+int findKthLargest(vector<int>& nums, int k) 
+    {
+        // Take a max heap
+        priority_queue<int> q;
+        // Insert element
+        // Element would be inserted in a manner that lagest element is at the top
+        for(int i=0;i<nums.size();i++)
+        {
+            q.push(nums[i]);
         }
-        return count;
-    }
-    
-    int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size();
-        int l = INT_MAX;
-        int h = INT_MIN;
-        int largest = n-k+1;  // i've find the kth smallest element from the begining 
-        for(int i=0;i<n;i++){
-            l = min(l,nums[i]);  // minimum num
-            h = max(h,nums[i]); // maximum num
+        // Take out the element k-1 times and kth element will be the required element
+        for(int i=0;i<k-1;i++)
+        {
+            q.pop();
         }
-        int ans = 0;
-        while(l<=h){
-            int m = (l+h)/2; 
-            if(fun(m,nums,n)<largest)
-                l = m+1;
-            else{
-                if(fun(m-1,nums,n)<largest){
-                    ans = m;
-                    break;
-                }
-                else
-                    h = m-1;
-            }
-        }
-        return ans;
+        //return that element
+        return q.top();
     }
 };
+    
+//     int fun(int x,vector<int>& nums,int n){
+//         int count = 0;
+//         for(int i=0;i<n;i++){
+//             if(nums[i] <= x)
+//                 count++;
+//         }
+//         return count;
+//     }
+    
+//     int findKthLargest(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int l = INT_MAX;
+//         int h = INT_MIN;
+//         int largest = n-k+1;  // i've find the kth smallest element from the begining 
+//         for(int i=0;i<n;i++){
+//             l = min(l,nums[i]);  // minimum num
+//             h = max(h,nums[i]); // maximum num
+//         }
+//         int ans = 0;
+//         while(l<=h){
+//             int m = (l+h)/2; 
+//             if(fun(m,nums,n)<largest)
+//                 l = m+1;
+//             else{
+//                 if(fun(m-1,nums,n)<largest){
+//                     ans = m;
+//                     break;
+//                 }
+//                 else
+//                     h = m-1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
