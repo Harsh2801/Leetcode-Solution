b293193790c8f05e0fe5bed2bc57aaef8b312df6
Harsh2801 <55402521+Harsh2801@users.noreply.github.com>
2022-06-23 13:41:40 +0530

    Time: 38 ms (14.01%), Space: 39.1 MB (6.52%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -30,8 +30,7 @@ public:
         //call recursion to the left and right child
         helper(root->left, sum-root->val, ans, v);
         helper(root->right, sum-root->val, ans, v);
-       //backtrack
-      //  v.pop_back();
+ 
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>> ans;
@@ -41,40 +40,3 @@ public:
     }
 };
 
-
-
-
-// // global 2d array to store result
-//     vector< vector<int> > res;
-    
-//     // helper function 
-//     void pathSum(TreeNode *root, int targetSum , vector<int> &v)
-//     {
-//         // if root is NULL 
-//         if( !root )
-//             return ;
-        
-//         // if we reached to leaf node and get the targetsum
-//         if( !root->left && !root->right && targetSum == root->val)
-//         {
-//             // push the lead node
-//             v.push_back(root->val);
-            
-//             // push the vector to result 
-//             res.push_back(v);
-            
-//             // backtrack 
-//             v.pop_back();
-//         }
-            
-            
-//         // push the current node value to the vector
-//         v.push_back(root->val);
-        
-//         // recursion to the left and right child
-//         pathSum(root->left, targetSum - root->val , v);
-//         pathSum(root->right, targetSum - root->val, v);
-        
-//         // backtrack 
-//         v.pop_back();
-//     }
